bdbbcaddc440c13c74942c93f482d1b5281eb373
Sőregi Krisztián <krisztiansoreg@gmail.com>
2023-02-25 18:50:26 +0100

    upgrade

---
+++ src/com/codecool/car_race/vehicles/Motorcycle.java
@@ -0,0 +1,30 @@
+package com.codecool.car_race.vehicles;
+
+import com.codecool.car_race.Race;
+import com.codecool.car_race.util.RandomHelper;
+
+public class Motorcycle extends Vehicle{
+    @Override
+    public void prepareForLap(Race race) {
+        actualSpeed = normalSpeed;
+
+        if(race.isRaining()){
+            int slowDown = RandomHelper.nextInt(5,50+1);
+            actualSpeed -= slowDown;
+        }
+    }
+
+    private static int motorcycleNumber = 1;
+
+    @Override
+    protected String generateName(){
+        return "Motorcycle" + motorcycleNumber++;
+    }
+
+
+    private static final int NORMAL_SPEED = 100;
+
+    public Motorcycle() {
+        super(NORMAL_SPEED);
+    }
+}
---
+++ src/com/codecool/car_race/vehicles/Truck.java
@@ -0,0 +1,62 @@
+package com.codecool.car_race.vehicles;
+
+import com.codecool.car_race.Race;
+import com.codecool.car_race.util.RandomHelper;
+
+public class Truck extends Vehicle{
+    @Override
+    public void prepareForLap(Race race) {
+        if (isBrokenDown()) actualSpeed = 0;
+        else actualSpeed = NORMAL_SPEED;
+
+        state = nextState();
+    }
+
+    @Override
+    protected String generateName(){
+        return Integer.toString(RandomHelper.nextInt( 1001));
+    }
+
+
+    private static final int NORMAL_SPEED = 100;
+
+    public Truck() {
+        super(NORMAL_SPEED);
+    }
+
+
+    private State state = State.OPERATIONAL;
+
+    private enum State {
+        OPERATIONAL,
+        BROKEN_DOWN
+    }
+
+    public boolean isBrokenDown() {
+        return state != State.OPERATIONAL;
+    }
+
+    public static final int BREAKDOWN_CHANCE = 5;
+
+    private static final int TURNS_NEEDED_TO_FIX_TRUCK = 2;
+
+    private int breakdownTurnsLeft;
+
+    private State nextState(){
+        switch(state){
+            case OPERATIONAL:
+                if (RandomHelper.eventWithChance(BREAKDOWN_CHANCE)){
+                    breakdownTurnsLeft = TURNS_NEEDED_TO_FIX_TRUCK;
+                    return State.BROKEN_DOWN;
+                }
+                break;
+            case BROKEN_DOWN:
+                if (breakdownTurnsLeft-- > 0){
+                    return State.BROKEN_DOWN;
+                }
+                break;
+        }
+        return State.OPERATIONAL;
+    }
+
+}
